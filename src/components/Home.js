b6c13f8db66149cd0d2ba639be018b3754ab25a9
alok gautam <agautam2285@gmail.com>
2023-01-15 11:09:44 +0530

    ddf

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+//import { Home } from "rect-router-dom"
+
+ function Home(){
+    return (
+         <div>
+          home 
+         </div>
+    )
+ }
+  export default Home
---
+++ src/components/login.js
@@ -0,0 +1,12 @@
+import { Link } from "rect-router-dom"
+
+function Login(){
+     return (
+        <div>
+            login page 
+            <Link to = "/home"> GO TO HOME </Link>
+        </div>
+     )
+}
+
+export default Login
